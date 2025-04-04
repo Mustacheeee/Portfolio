d97909826218420316fb666f8dd7d98e4ab0c3e0
Yixin Zhao <st97334@gmail.com>
2025-04-04 18:52:46 -0400

    Update Landing.tsx

--- data-story-portfolio/frontend/src/components/Landing.tsx
+++ data-story-portfolio/frontend/src/components/Landing.tsx
@@ -13,7 +13,7 @@ const Landing = () => {
       >
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-25 bottom-32 w-full flex justify-center items-center'>
         <a href='#personalInfo'>

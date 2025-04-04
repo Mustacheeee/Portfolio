b0e36b87ec1d0ce6797b956ed854f36da1d2cc7d
Yixin Zhao <st97334@gmail.com>
2025-04-04 19:13:05 -0400

    trial

--- data-story-portfolio/frontend/src/components/PersonalInfo.tsx
+++ data-story-portfolio/frontend/src/components/PersonalInfo.tsx
@@ -102,7 +102,7 @@ const PersonalInfo = () => {
       >
         <div className="personal-info bg-bigyellow mr-10 sm:w-1/3 xs:w-1/2">
           <img 
-            src="/mypic.jpg"
+            src="../public/mypic.jpg"
             alt="Your Name"
             className="profile-pic"
           />
